4e70bca37ddb38a9bc865f192d80126923b4b959
abhishektada <121951555+abhishektada@users.noreply.github.com>
2023-03-15 17:40:44 +0530

    cross origin allow change

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ app.use(
 );
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  let origin = req.headers.origin;
+  res.setHeader("Access-Control-Allow-Origin", origin);
   res.header(
     "Access-Control-Allow-headers",
     "Origin, X-Requested-With,Content-Type,auth-token,Accept"
